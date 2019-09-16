ad9a3b0ce81bccc36e295830e182717976e17870
Adam Kendis <atkendis@gmail.com>
2019-09-16 12:54:16 -0700

    Knex and knexfile set up.

---
+++ knexfile.js
@@ -0,0 +1,40 @@
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgresql://localhost/fish_keeper',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/development'
+    },
+    useNullAsDefault: true
+  },
+
+  staging: {
+    client: 'pg',
+    connection: 'postgresql://localhost/fish_keeper_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/production'
+    },
+    useNullAsDefault: true
+  }
+
+};
