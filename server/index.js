536eba0e29c6383ad193ab8899fbdc7ac2a8e546
Adam Kendis <atkendis@gmail.com>
2019-09-19 00:17:36 -0700

    Basic routes stubbed out.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const environment = process.env.NODE_ENV || 'development';
+const config = require('../knexfile.js')[environment];
+const db = require('knex')(config);
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,4 +11,30 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(port => console.log(`Listening on port ${port}`));
+app.get('/', (req, res) => {
+  db('fish_catch_data')
+    .then(data => {
+      res.send(data);
+    });
+});
+
+app.post('/catch', (req, res) => {
+  //
+})
+
+app.put('/catch/:fishId', (req, res) => {
+  const fishId = req.params.fishId;
+  //
+})
+
+app.delete('/catch/:fishId', (req, res) => {
+  //
+})
+
+app.get('/history', (req, res) => {
+  //
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
