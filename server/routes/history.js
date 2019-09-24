75abe18e06efc3b72d193280d58e18248a2905d0
Adam Kendis <atkendis@gmail.com>
2019-09-24 13:18:46 -0700

    Added post handler to HistoryController.

--- server/routes/history.js
+++ server/routes/history.js
@@ -1,19 +1,30 @@
 const express = require('express');
 const HistoryController = express.Router();
 const environment = process.env.NODE_ENV || 'development';
-const config = require('../knexfile.js')[environment];
+const config = require('../../knexfile.js')[environment];
 const db = require('knex')(config);
 
-HistoryController.route('/history')
+HistoryController.route('/?')
   .get(function(req, res, next){
     db('fish_catch_data')
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      console.error(err);
-    })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).end();
+      })
   })
   .post(function(req, res, next){
-    //
+    db('fish_catch_data')
+      .insert(req.data)
+      .then(() => {
+        res.status(200).send({message: 'Fish saved to database.'});
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).end();
+      })
   })
+
+module.exports = HistoryController;
