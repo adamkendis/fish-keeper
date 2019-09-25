df7049a83cbe796d2377638f178abf4a9073c59c
Adam Kendis <atkendis@gmail.com>
2019-09-25 12:41:05 -0700

    Removed unnecessary code and comments from server index.

--- server/index.js
+++ server/index.js
@@ -1,48 +1,13 @@
 const express = require('express');
 const path = require('path');
-const environment = process.env.NODE_ENV || 'development';
-const config = require('../knexfile.js')[environment];
-const db = require('knex')(config);
-
 const app = express();
 const port = process.env.PORT || 3000;
-// const routes = require('./routes');
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 require('./routes')(app);
-
-// app.get('/', (req, res) => {
-//   //
-// })
-
-// app.post('/catches', (req, res) => {
-//   //
-// })
-
-// app.put('/catches/:fishId', (req, res) => {
-//   const fishId = req.params.fishId;
-//   //
-// })
-
-// app.delete('/catches/:fishId', (req, res) => {
-//   const fishId = req.params.fishId;
-//   db('fish_catch_data')
-//     .where({ id: fishId })
-//     .del()
-//     .then(() => {
-//       res.send('Deleted')
-//     })
-//     .catch(err => {
-//       console.error(err);
-//     })
-// })
-
-// app.get('/history', (req, res) => {
-//   //
-// })
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
