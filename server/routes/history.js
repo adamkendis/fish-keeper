0c861181e0256234219ec77e78a353ce72bfe93b
Adam Kendis <atkendis@gmail.com>
2019-09-24 23:42:53 -0700

    Delete on history route implemented.

--- server/routes/history.js
+++ server/routes/history.js
@@ -5,7 +5,7 @@ const config = require('../../knexfile.js')[environment];
 const db = require('knex')(config);
 
 HistoryController.route('/?')
-  .get(function(req, res, next){
+  .get(function(req, res){
     db('fish_catch_data')
       .then(data => {
         res.send(data);
@@ -15,11 +15,24 @@ HistoryController.route('/?')
         res.status(500).end();
       })
   })
-  .post(function(req, res, next){
+  .post(function(req, res){
     db('fish_catch_data')
       .insert(req.data)
       .then(() => {
         res.status(200).send({message: 'Fish saved to database.'});
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).end();
+      })
+  })
+  .delete(function(req, res){
+    let fishId = req.body.fish.id;
+    db('fish_catch_data')
+      .where({ id: fishId })
+      .del()
+      .then(() => {
+        res.send('Deleted')
       })
       .catch(err => {
         console.error(err);
