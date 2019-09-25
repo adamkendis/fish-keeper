094963749d578257b71acea89ff9755c5c0ddf9b
Adam Kendis <atkendis@gmail.com>
2019-09-25 12:40:16 -0700

    Moved route handler funcs to controllers dir.

--- server/routes/history.js
+++ server/routes/history.js
@@ -1,59 +1,16 @@
 const express = require('express');
 const HistoryController = express.Router();
-const environment = process.env.NODE_ENV || 'development';
-const config = require('../../knexfile.js')[environment];
-const db = require('knex')(config);
+const { getAllFish, 
+        getFishById, 
+        updateFishById, 
+        deleteFishById } = require('../controllers/historyHandlers.js');
 
 HistoryController.route('/')
-  .get(function(req, res){
-    db('fish_catch_data')
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        console.error(err);
-        res.status(500).end();
-      })
-  })
+  .get(getAllFish)
 
 HistoryController.route('/:id')
-  .get(function(req, res){
-    let id = req.params.id
-    db('fish_catch_data')
-      .where({ id })
-      .then((data) => {
-        res.send(data);
-      })
-      .catch(err => {
-        console.error(err)
-        res.status(500).end();
-      })
-  })
-  .put(function(req, res){
-    let id = req.params.id;
-    db('fish_catch_data')
-      .where({ id })
-      .update(req.body.fishData)
-      .then(() => {
-        res.send('Updated');
-      })
-      .catch(err => {
-        console.error(err);
-        res.status(500).end();
-      })
-  })
-  .delete(function(req, res){
-    let id = req.params.id;
-    db('fish_catch_data')
-      .where({ id })
-      .del()
-      .then(() => {
-        res.send('Deleted')
-      })
-      .catch(err => {
-        console.error(err);
-        res.status(500).end();
-      })
-  })
+  .get(getFishById)
+  .put(updateFishById)
+  .delete(deleteFishById)
 
 module.exports = HistoryController;
