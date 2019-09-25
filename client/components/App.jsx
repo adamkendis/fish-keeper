bfd7f75dca7c143730b11c5f48b4f211af0c934f
Adam Kendis <atkendis@gmail.com>
2019-09-25 15:45:21 -0700

    Barebones App component, client index.js.

---
+++ client/components/App.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
+
+}
+
+export default App;
