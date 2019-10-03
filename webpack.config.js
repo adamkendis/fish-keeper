5bc05b70437cc63e4c7c69d0bc870c5bb2b27d97
Adam Kendis <atkendis@gmail.com>
2019-10-02 21:28:58 -0700

    Bundle analyzer.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
   entry: './client/index.js',
@@ -11,6 +12,9 @@ module.exports = {
       }
     ]
   },
+  plugins: [ 
+    new BundleAnalyzerPlugin()
+  ],
   resolve: {
     extensions: ['*', '.js', '.jsx']
   },
