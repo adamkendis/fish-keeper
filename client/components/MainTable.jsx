0461a7251a6a5ec8a9fdbb7ab84c265093446c76
Adam Kendis <atkendis@gmail.com>
2019-10-02 21:29:19 -0700

    Basic MainTable functionality.

--- client/components/MainTable.jsx
+++ client/components/MainTable.jsx
@@ -1,5 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, forwardRef } from 'react';
 import MaterialTable from 'material-table';
+import ArrowUpward from '@material-ui/icons/ArrowUpward';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+import axios from 'axios';
 
 class MainTable extends Component {
   constructor(props) {
@@ -8,11 +17,33 @@ class MainTable extends Component {
       fish: [],
     };
   }
+
+  componentDidMount() {
+    axios.get('/history')
+      .then(res => {
+        this.setState({ fish: res.data })
+      })
+  }
+
   render() {
+
+    const tableIcons = {
+      Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+      FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+      LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+      NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+      PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+      ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+      Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+      SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />),
+      ViewColumns: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+    };
+
     return (
       <div>
         <MaterialTable 
           title="Catch History"
+          icons={tableIcons}
           columns={[
             { title: "Species", field: "fish_species" },
             { title: "Length", field: "fish_length", type: "numeric" },
@@ -20,7 +51,7 @@ class MainTable extends Component {
             { title: "Hook Size", field: "hook_size" },
             { title: "Latitude", field: "latitude" },
             { title: "Longitude", field: "longitude" },
-          ]},
+          ]}
           data={this.state.fish}
         
         />
@@ -28,3 +59,5 @@ class MainTable extends Component {
     )
   }
 }
+
+export default MainTable;
