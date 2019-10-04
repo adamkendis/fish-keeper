cdca46fb62648f441a0cc60fed309bec9cc1f07e
Adam Kendis <atkendis@gmail.com>
2019-10-03 23:40:21 -0700

    Added /catch route to Main.

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import MainTable from './MainTable';
+import CatchView from './CatchView';
 
 const Main = () => {
   return (
     <main>
       <Switch>
         <Route path='/map' component={MainTable} />
-        {/* <Route path='/catch' component={<div>Catch</div>} /> */}
+        <Route path='/catch' component={CatchView} />
         {/* <Route path='graphs' component={<div>Graphs</div>} /> */}
       </Switch>
     </main>
